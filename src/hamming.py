514156da53708e4695a8a000ce4f9b0c95b9acb6
Marek Połom <marekpolom22@gmail.com>
2020-11-02 18:50:17 +0100

    hamming function for test 5

--- src/hamming.py
+++ src/hamming.py
@@ -1,5 +1,8 @@
 def distance(dist_1, dist_2):
-    if dist_1 == dist_2:
-        return 0
-    elif dist_1 != dist_2:
-        return 1
+    j = 0
+
+    for i in range(0, len(dist_1)):
+        if dist_1[i] != dist_2[i]:
+            j+=1
+
+    return j
