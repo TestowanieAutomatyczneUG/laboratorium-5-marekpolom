c3755bed62546a7997f357210256fc4ad10befa1
Marek Połom <marekpolom22@gmail.com>
2020-11-02 22:27:08 +0100

    test 11 roman

--- tests/test_roman.py
+++ tests/test_roman.py
@@ -33,3 +33,6 @@ class RomanNumeralsTest(unittest.TestCase):
 
     def test_50_is_a_single_l(self):
         self.assertEqual(roman(59), "LIX")
+
+    def test_90_being_100_10_is_xc(self):
+        self.assertEqual(roman(93), "XCIII")
