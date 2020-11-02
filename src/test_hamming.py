d72825596d4c776fd821b0bcfc6e6004d94dc090
Marek Połom <marekpolom22@gmail.com>
2020-11-02 18:42:38 +0100

    test 2 hamming

--- src/test_hamming.py
+++ src/test_hamming.py
@@ -5,6 +5,9 @@ class HammingTest(unittest.TestCase):
     def test_empty_strands(self):
         self.assertEqual(hamming.distance("", ""), 0)
 
+    def test_single_letter_identical_strands(self):
+        self.assertEqual(hamming.distance("A", "A"), 0)
+
     # Utility functions
     def setUp(self):
         try:
