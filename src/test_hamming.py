f9328e6176ad35761e35307d8dec4e693cab8bf5
Marek Połom <marekpolom22@gmail.com>
2020-11-02 18:46:30 +0100

    test 5 hamming

--- src/test_hamming.py
+++ src/test_hamming.py
@@ -14,6 +14,9 @@ class HammingTest(unittest.TestCase):
     def test_long_identical_strands(self):
         self.assertEqual(hamming.distance("GGACTGAAATCTG", "GGACTGAAATCTG"), 0)
 
+    def test_long_different_strands(self):
+        self.assertEqual(hamming.distance("GGACGGATTCTG", "AGGACGGATTCT"), 9)
+
     # Utility functions
     def setUp(self):
         try:
