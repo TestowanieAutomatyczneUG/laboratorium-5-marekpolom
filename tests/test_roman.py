98890e17f52de5fc481f1a3a542a5b352b48296f
Marek Połom <marekpolom22@gmail.com>
2020-11-02 19:32:59 +0100

    test 5 roman

--- tests/test_roman.py
+++ tests/test_roman.py
@@ -15,3 +15,6 @@ class RomanNumeralsTest(unittest.TestCase):
 
     def test_4_being_5_1_is_iv(self):
         self.assertEqual(roman(4), "IV")
+
+    def test_5_is_a_single_v(self):
+        self.assertEqual(roman(5), "V")
