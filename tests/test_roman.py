58367853ae7eb99bdf3cdf13d5893a694d854fdb
Marek Połom <marekpolom22@gmail.com>
2020-11-02 19:30:22 +0100

    test 2 roman

--- tests/test_roman.py
+++ tests/test_roman.py
@@ -1,6 +1,11 @@
 import unittest
-import roman from src.roman
+import src.roman
+
+roman = src.roman.roman
 
 class RomanNumeralsTest(unittest.TestCase):
     def test_1_is_a_single_i(self):
         self.assertEqual(roman(1), "I")
+
+    def test_2_is_two_i_s(self):
+        self.assertEqual(roman(2), "II")
