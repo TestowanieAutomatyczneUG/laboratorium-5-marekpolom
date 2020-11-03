0e3029cb0f4c914dc87a2f87ff4358868ae3e4bc
Marek Połom <marekpolom22@gmail.com>
2020-11-03 15:04:14 +0100

    test 2 song

--- tests/test_song.py
+++ tests/test_song.py
@@ -6,3 +6,6 @@ song = src.song.song
 class HammingTest(unittest.TestCase):
     def test_song_first_day(self):
         self.assertEqual(song(1), "On the first day of Christmas my true love gave to me: a Partridge in a Pear Tree.")
+
+    def test_song_second_day(self):
+        self.assertEqual(song(2), "On the second day of Christmas my true love gave to me: two Turtle Doves, and a Partridge in a Pear Tree.")
