5af88157cac49ee311bd4a6da008e42f59216880
Marek Połom <marekpolom22@gmail.com>
2020-11-03 15:06:50 +0100

    test 4 song

--- tests/test_song.py
+++ tests/test_song.py
@@ -12,3 +12,6 @@ class HammingTest(unittest.TestCase):
 
     def test_song_third_day(self):
         self.assertEqual(song(3), "On the third day of Christmas my true love gave to me: three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.")
+    
+    def test_song_forth_day(self):
+        self.assertEqual(song(4), "On the fourth day of Christmas my true love gave to me: four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.")
