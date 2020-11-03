28ea977a21cdf108c293f86f422d9687568f9a7e
Marek Połom <marekpolom22@gmail.com>
2020-11-03 15:10:02 +0100

    function song for test 6

--- src/song.py
+++ src/song.py
@@ -9,4 +9,6 @@ def song(n):
         return('On the fourth day of Christmas my true love gave to me: four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.')
     elif n == 5:
         return('On the fifth day of Christmas my true love gave to me: five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.')
+    elif n == 6:
+        return('On the sixth day of Christmas my true love gave to me: six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.')
     
