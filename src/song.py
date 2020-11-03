e9bdafd94fd418d4cb581a67719deb695238c577
Marek Połom <marekpolom22@gmail.com>
2020-11-03 15:04:20 +0100

    function song for test 2

--- src/song.py
+++ src/song.py
@@ -1,3 +1,6 @@
 def song(n):
     if n == 1:
         return('On the first day of Christmas my true love gave to me: a Partridge in a Pear Tree.')
+    elif n == 2:
+        return('On the second day of Christmas my true love gave to me: two Turtle Doves, and a Partridge in a Pear Tree.')
+    
