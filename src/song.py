b5a114e1de69cd333db8c3c0ee49f13ce2ca0fac
Marek Połom <marekpolom22@gmail.com>
2020-11-03 16:10:15 +0100

    function song for test multiple lines 1

--- src/song.py
+++ src/song.py
@@ -1,26 +1,15 @@
-def song(n):
-    if n == 1:
-        return('On the first day of Christmas my true love gave to me: a Partridge in a Pear Tree.')
-    elif n == 2:
-        return('On the second day of Christmas my true love gave to me: two Turtle Doves, and a Partridge in a Pear Tree.')
-    elif n == 3:
-        return('On the third day of Christmas my true love gave to me: three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.')
-    elif n == 4:
-        return('On the fourth day of Christmas my true love gave to me: four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.')
-    elif n == 5:
-        return('On the fifth day of Christmas my true love gave to me: five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.')
-    elif n == 6:
-        return('On the sixth day of Christmas my true love gave to me: six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.')
-    elif n == 7:
-        return('On the seventh day of Christmas my true love gave to me: seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.')
-    elif n == 8:
-        return('On the eighth day of Christmas my true love gave to me: eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.')
-    elif n == 9:
-        return('On the ninth day of Christmas my true love gave to me: nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.')
-    elif n == 10:
-        return('On the tenth day of Christmas my true love gave to me: ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.')
-    elif n == 11:
-        return('On the eleventh day of Christmas my true love gave to me: eleven Pipers Piping, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.')
-    elif n == 12:
-        return('On the twelfth day of Christmas my true love gave to me: twelve Drummers Drumming, eleven Pipers Piping, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.')
-    
+class Song():
+    def __init__(self):
+        self.file = open('src/files/song.txt', '+r')
+        self.f = self.file.read().splitlines()
+
+    def read(self, n, m=0):
+        if m == 0:
+            return(self.f[n-1])
+        else:
+            return(self.f[n-1: m])
+
+    def close(self):
+        self.file.close()
+
+# print(song(1, 12))
