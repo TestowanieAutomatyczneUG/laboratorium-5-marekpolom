1f7bbc42e7bdd56b0f3ea3ad7aa9e490ba913239
Marek Połom <marekpolom22@gmail.com>
2020-11-03 17:45:41 +0100

    function song for test exception 3

--- src/song.py
+++ src/song.py
@@ -5,6 +5,9 @@ class Song():
 
     def read(self, n, m=0):
         try:
+            if n <= 0 or m < 0:
+                raise Exception("Values can't be lower than 1")
+
             if int(m) == 0:
                 return(self.f[int(n)-1])
             else:
